a57ebf84e2f25e5ff519919676b54b43a322bb0a
mohamed mouine <mohamedmouine@MacBook-Air-de-mohamed.local>
2019-12-04 20:04:05 -0500

    fix lab10

--- lab10/model/task.js
+++ lab10/model/task.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema();
+
 taskSchema.add({
     name: String,
-    userId: String
+    userId: String,
 });
 
-taskSchema.methods.toDTO = function () {
+taskSchema.methods.toDTO = function() {
     const obj = this.toObject();
 
-    const dto = {
+    return {
         id: obj._id,
         name: obj.name
-    };
-
-    return dto;
-};
+    }
+}
 
 const Task = mongoose.model('Task', taskSchema);
 
--- lab10/taskRouter.js
+++ lab10/taskRouter.js
@@ -1,38 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/LAB10', 
+    { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.Promise = global.Promise;
 
 taskRouter = express.Router();
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/LAB10', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = global.Promise;
 
 const Task = require('./model/task.js').model;
 const User = require('./model/user.js').model;
 
+const SERVICE_UNAVAILABLE = 503;
+
 taskRouter.post('/users', (_, res) => {
     const user = new User();
 
-    user.save( (err, user) => {
-        if (!!err)
-            return res.sendStatus(503);
-        console.log("Saving user with id : " + user._id)
-        return res.status(200).send(JSON.stringify({id: user._id}))
+    user.save((err, user) => {
+        if (!!err) {
+            console.error('Error while creating user in database : ' + err)
+            return res.sendStatus(SERVICE_UNAVAILABLE);
+        }
+        console.log('Creating user with id : ' + user._id);
+        return res.status(201).send(JSON.stringify({id: user._id}))
     })
 });
 
 taskRouter.post('/:userId/tasks', (req, res) => {
     const userId = req.params.userId;
-    const taskRequest = req.body;
     validateUserId(userId, res, () => {
-        validateTask(taskRequest, res, () => {
-          const task = new Task({name: taskRequest.name, userId: userId});
-          task.save((err, task) => {
-              if (err) {
-                  return res.sendStatus(503);
-              }
-              
-              return res.status(200).send(JSON.stringify(task.toDTO()));
-          });
+        validateTask(req.body, res, () => {
+            const task = new Task({name: req.body.name, userId})
+            task.save( (err, task) => {
+                if (!!err) {
+                    console.error('Error while creating task in database : ' + err)
+                    return res.sendStatus(SERVICE_UNAVAILABLE);
+                }
+                console.log('Created task in database with id : ' + task._id)
+                return res.status(201).send(JSON.stringify(task.toDTO()));
+            })
         })
     })
 });
@@ -40,13 +46,16 @@ taskRouter.post('/:userId/tasks', (req, res) => {
 taskRouter.get('/:userId/tasks', (req, res) => {
     const userId = req.params.userId;
     validateUserId(userId, res, () => {
-      Task.find({userId}, (err, tasks) => {
-        if (!!err)
-          return res.sendStatus(503);
-        tasks = tasks || [];
-        tasks = tasks.map((t) => t.toDTO());
-        return res.status(200).send(JSON.stringify({tasks}))
-      })
+        Task.find({userId}, (err, tasks) => {
+            if (!!err) {
+                console.error('Error while getting all tasks in database : ' + err)
+                return res.sendStatus(SERVICE_UNAVAILABLE);
+            }
+            tasks = tasks || [];
+            tasks = tasks.map((task) => task.toDTO());
+            console.log('Fetching all tasks from database')
+            return res.status(200).send(JSON.stringify({tasks}));
+        })
     })
 });
 
@@ -57,23 +66,24 @@ taskRouter.put('/:userId/tasks/:taskId', (req, res) => {
 
     validateUserId(userId, res, () => {
         validateUpdateRequest(updatedTask, res, () => {
-          Task.findById(taskId, (err, task) => {
-            if (!task) {
-                return res.status(404).send('Task with id \'' + taskId + '\' doesn\'t exist.');
-            }
-            if (!!err) {
-              console.error(err);
-              return res.sendStatus(503);
-            }
-            task.name = updatedTask.name;
-            task.save((err, task) => {
-                if (err) {
-                    return res.sendStatus(503);
+            Task.findById(taskId, (err, task) => {
+                if (!task) {
+                    return res.status(404).send(`Task with id : ${taskId} does not exist.`);
                 }
-                
-                return res.status(200).send(JSON.stringify(task.toDTO()));
-            });
-        });
+                if (!!err) {
+                    console.error('Error while getting task in database : ' + err)
+                    return res.sendStatus(SERVICE_UNAVAILABLE);
+                }
+                task.name = updatedTask.name;
+                task.save((err, task) => {
+                    if (!!err) {
+                        console.error('Error while modifying task in database : ' + err)
+                        return res.sendStatus(SERVICE_UNAVAILABLE)
+                    }
+                    console.log('Modifyed task in database with id : ' + taskId);
+                    return res.sendStatus(200);
+                })
+            })
         })
     })
 });
@@ -81,38 +91,39 @@ taskRouter.put('/:userId/tasks/:taskId', (req, res) => {
 taskRouter.delete('/:userId/tasks/:taskId', (req, res) => {
     const { userId, taskId } = req.params;
     validateUserId(userId, res, () => {
-      console.log(taskId)
-      Task.findOneAndRemove(taskId, (err, task) => {
-        if (!task) {
-          return res.status(404).send('Task with id \'' + taskId + '\' doesn\'t exist.');
-        }
-        if (!!err) {
-          console.error(err);
-          return res.sendStatus(503);   
-        }
-        console.log(task)
-        return res.sendStatus(204);
+        Task.findByIdAndRemove(taskId, (err, task) => {
+            console.log
+            if (!task) {
+                return res.sendStatus(404);
+            }
+            if (!!err) {
+                console.error('Error while deleting task in database : ' + err)
+                return res.sendStatus(SERVICE_UNAVAILABLE);
+            }
+            console.log('Deleted task in database with id : ' + taskId);
+            return res.sendStatus(200);
+        })
     })
-  })
 });
 
 const validateUserId = (userId, res, next) => {
-  User.findById(userId, (err, user) => {
-    if (!user) {
-      return res.status(404).send('User with id \'' + userId + '\' doesn\'t exist.');
-    }
-    if (!!err)
-      return res.sendStatus(503);
-    next();
-  })
-;};
+    User.findById(userId, (err, user) => {
+        if (!user) {
+            res.status(404).send(`User with id : ${userId} does not exist`)
+        }
+        if (!!err) {
+            console.error('Error while getting user in database : ' + err)
+            return res.sendStatus(SERVICE_UNAVAILABLE)
+        }
+        next()
+    })
+};
 
 const validateTask = (task, res, next) => {
-  if (!task.name || !task.name || task.name === '') {
-    return res.status(400).send('Task definition is invalid.');
-  }
-
-  next();
+    if (!!task && !!task.name && task.name !== '') {
+        return next();
+    }
+    res.status(400).send(`Task definition is not valid`)
 };
 
 const validateUpdateRequest = (updatedTask, res, next) => {
